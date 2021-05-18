7422197297d73a9bad942834f505069787b19f0b
metacringer <metacringer@gmail.com>
2021-05-19 00:05:25 +0300

    change passowrd in config

--- include/dbconfig.h
+++ include/dbconfig.h
@@ -3,7 +3,7 @@
 
 static char * dbhost  = "localhost";
 static char * dbuser = "forum";
-static char * dbpasswd = "1111";
+static char * dbpasswd = "21436587";
 static char * dbname = "FORUM";
 static unsigned long dbport = 3306;
 static char * dbusocket = NULL;
